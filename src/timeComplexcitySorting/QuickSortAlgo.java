bf925ca15003718df8c9648646cda85872e5ccea
Vishnumdas589 <Vishnu.mdas@gmail.com>
2025-07-09 01:51:59 +0530

    quicksort1

---
+++ src/timeComplexcitySorting/QuickSortAlgo.java
@@ -0,0 +1,57 @@
+package timeComplexcitySorting;
+
+public class QuickSortAlgo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] arr= {64,34,25,12,22,11,90};
+		quickSort(arr,0,arr.length-1);
+		for(int x : arr) {
+			
+			System.out.println(x);
+		}
+
+
+	}
+	public static void quickSort(int [] arr, int l, int r) {
+		while(l<r) {
+			int pivort = partition(arr,l,r);
+			quickSort(arr,l,pivort);
+			quickSort(arr,pivort+1,r);
+		}
+		
+	}
+	public static int partition(int [] arr, int l, int r) {
+		int i =l;
+		int j = r;
+		
+		int pivort  = arr[l];
+		
+		while(i<=j) {
+			
+			while (arr[i] <= pivort && i<r) {
+				i++;
+			}
+			
+			
+			while (arr[j] > pivort && j >= 0 ) {
+				
+				j--;
+				
+			}
+			
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] =temp;	
+			
+		}
+		if(i > j) {
+			int temp2 = arr[l];
+			arr[l] = arr[j];
+			arr[j] =temp2;	
+		}
+		
+		return j;
+	}
+
+}
