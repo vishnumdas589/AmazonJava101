6cf796ddf02e1fe38f12937203b6873753ebd674
Vishnumdas589 <Vishnu.mdas@gmail.com>
2025-07-09 13:56:04 +0530

    completed QuickSort

--- src/timeComplexcitySorting/QuickSortAlgo.java
+++ src/timeComplexcitySorting/QuickSortAlgo.java
@@ -1,23 +1,22 @@
 package timeComplexcitySorting;
-
 public class QuickSortAlgo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [] arr= {64,34,25,12,22,11,90};
+		int [] arr= {64,34,25,12,22};
 		quickSort(arr,0,arr.length-1);
 		for(int x : arr) {
 			
 			System.out.println(x);
 		}
 
-
 	}
 	public static void quickSort(int [] arr, int l, int r) {
-		while(l<r) {
+		if(l<r) {
 			int pivort = partition(arr,l,r);
-			quickSort(arr,l,pivort);
+			quickSort(arr,l,pivort-1);
 			quickSort(arr,pivort+1,r);
+			
 		}
 		
 	}
@@ -26,30 +25,32 @@ public class QuickSortAlgo {
 		int j = r;
 		
 		int pivort  = arr[l];
-		
-		while(i<=j) {
+	
+		while(i<j) {
 			
-			while (arr[i] <= pivort && i<r) {
+			while (arr[i] <= pivort && i < r) {
 				i++;
 			}
 			
 			
-			while (arr[j] > pivort && j >= 0 ) {
+			while (arr[j] > pivort && j > l ) {
 				
 				j--;
 				
 			}
-			
-			int temp = arr[i];
-			arr[i] = arr[j];
-			arr[j] =temp;	
+			if(i < j) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] =temp;	
+			}
+	
 			
 		}
-		if(i > j) {
-			int temp2 = arr[l];
-			arr[l] = arr[j];
-			arr[j] =temp2;	
-		}
+		int temp2 = arr[l];
+		arr[l] = arr[j];
+		arr[j] =temp2;	
+
+		
 		
 		return j;
 	}
